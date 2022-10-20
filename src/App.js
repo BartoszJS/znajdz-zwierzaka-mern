8ae16472a26edc72e142d2ac6557f6c951c72789
BartoszJS <bartosz290300@wp.pl>
2022-10-20 17:54:28 +0200

    2 logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Logo from "./components/Logo.js";
+import Landing from "./pages/Landing";
+
 function App() {
   return (
     <div>
       <h1>ZNAJDZ ZWIERZAKA</h1>
+      <Landing />
+      <Logo />
     </div>
   );
 }
